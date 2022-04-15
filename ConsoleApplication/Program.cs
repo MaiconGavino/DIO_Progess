1f53f867db0cd9564f215bd6c7e43a2ed1ca54de
gavino <maiconserrao57@gmail.com>
2022-04-15 14:14:15 -0400

    Ordenando Vetor

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = {1, 5, 6, 7, 3, 5, 4, 7, 9, 2};
+            for (int i = 0; i < Sort(arr).Length; i++)
+            {
+                Console.Write(Sort(arr)[i]);
+            }
+        }
+
+        public static int[] Sort(int[] array)
+        {
+            int tam = array.Length;
+            int first = array[0];
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = i + 1; j < tam; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        first = array[i];
+                        array[i] = array[j];
+                        array[j] = first;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+    }
+}
